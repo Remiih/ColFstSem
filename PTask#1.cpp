fa25719fd6697c61c0f7fad72f5536ba36a20b9a
Jeremiii <jj.jeremy09@gmail.com>
2023-09-07 10:08:44 +0800

    new file:   .vscode/launch.json 	new file:   .vscode/settings.json 	new file:   .vscode/tasks.json 	new file:   PTask#1.PNG 	new file:   PTask#1.cpp 	new file:   PTask#1.exe 	new file:   PTask#2.cpp 	new file:   PTask#2.exe 	new file:   PTask#2.png 	new file:   PTask#3.cpp 	new file:   PTask#3.exe 	new file:   PTask#3.png 	new file:   ascii_image.txt 	new file:   icons_72915.png 	new file:   image to ascii.py

---
+++ PTask#1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    cout << "Last Name: Ramos\nFirst Name: John Jeremy\nMiddle Name: Parreno\nCourse: BS Computer Engineering\nEmail Adress: remi1213.saw@gmail.com\nAddress: Taguig City\nTelephone No: 09206622348\n";
+    system("pause");
+    return 0;
+}

e7a6dd27910f8ec170af06041ce1027975341940
Jeremiii <38002650+Remiih@users.noreply.github.com>
2023-09-16 20:14:52 +0800

    update Average.cpp and Average.exe

--- 2/Average.cpp
+++ 2/Average.cpp
@@ -3,23 +3,25 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
     float firstNumber;
     float secondNumber;
     cout << "Input your first number: ";
     cin >> firstNumber;
     cout << "\nInput your second number: ";
     cin >> secondNumber;
-    
+
     cout << "\nSum of the numbers: ";
-    cout << setprecision(3) << (firstNumber+secondNumber) << endl;
+    cout << setprecision(3) << (firstNumber + secondNumber) << endl;
     cout << "\nDifference of the numbers: ";
-    cout << setprecision(3) << (firstNumber-secondNumber) << endl;
+    cout << setprecision(3) << (firstNumber - secondNumber) << endl;
     cout << "\nProduct of the numbers: ";
-    cout << setprecision(3) << (firstNumber*secondNumber) << endl;
+    cout << setprecision(3) << (firstNumber * secondNumber) << endl;
     cout << "\nQuotient of the numbers: ";
-    cout << setprecision(3) << (firstNumber/secondNumber) << endl;
+    cout << setprecision(3) << (firstNumber / secondNumber) << endl;
     cout << "\nAverage of the numbers: ";
-    cout << setprecision(3) << ((firstNumber+secondNumber)/2) << endl;
+    cout << setprecision(3) << ((firstNumber + secondNumber) / 2) << endl;
+
     return 0;
 }
